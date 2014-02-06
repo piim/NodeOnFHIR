80e0376263ee0de8da4234112b6d09278fe5ca10
npedrini <go@looklisten.net>
2014-02-06 15:15:02 -0500

    bash script corrupted

--- import/load.sh
+++ import/load.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 which node
-echo "Would you like to clear the definitions/conditions collections [Y/N]?"
 
+echo "Would you like to clear the definitions/conditions collections [Y/N]?"
 read clear
 
-echo -n "Loading definitions...."
+echo -n "Loading definitions..."
 out=`node definitions/load-definitions $clear`
+
 if [ $? -ne 0 ] ; then
-    echo "failed."
-    exit 127
+  echo "failed"
+  echo 127
 else
-    echo "ok."
+  echo "ok"
 fi
 
-echo -n "Loading conditions...."
+echo -n "Loading conditions..."
 out=`node conditions/load-conditions $clear`
+
 if [ $? -ne 0 ] ; then
-    echo "failed."
-    exit 127
+  echo "failed"
+  echo 127
 else
-    echo "ok."
+  echo "ok"
 fi
 
-echo "done.
-"
+echo "done"
