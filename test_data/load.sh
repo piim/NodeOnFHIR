4ba0e560aa4cd23f0a75b5f6ea748eb0c6deecef
npedrini <go@looklisten.net>
2014-02-26 11:55:52 -0500

    removed import of observation, patient and practitioner test data

--- test_data/load.sh
+++ test_data/load.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 which node
-echo -n "loading observations...."
-out=`node load list observation`
-if [ $? -ne 0 ] ; then
-    echo "failed."
-    exit 127
-else
-    echo "ok."
-fi
-
-echo -n "loading patients...."
-out=`node load patient patient`
-if [ $? -ne 0 ] ; then
-    echo "failed."
-    exit 127
-else
-    echo "ok."
-fi
-
-echo -n "loading practitioners...."
-out=`node load practitioner practitioner`
-if [ $? -ne 0 ] ; then
-    echo "failed."
-    exit 127
-else
-    echo "ok."
-fi
 
 echo -n "loading medications...."
 out=`node load medication medication`
